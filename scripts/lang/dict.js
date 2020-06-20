39be096347c19bd5cf21fa0fbb8e768c5b3232a4
Tobias Kärst <tobias.kaerst@gmx.net>
2020-06-20 23:28:48 +0200

    Added feature to change the language.

---
+++ scripts/lang/dict.js
@@ -0,0 +1,50 @@
+var dictionary = {
+    'sorting_algorithm_visualisation': {
+        'de': 'Visualisierung Sortieralgorithmen',
+        'en': 'Sorting Algorithm Visualisation',
+    },
+    'amount_of_elements': {
+        'de': 'Anzahl der Elemente',
+        'en': 'Amount of elements',
+    },
+    'arrangement_of_elements': {
+        'de': 'Anordnung der Elemente',
+        'en': 'Arrangement of elements',
+    },
+    'random': {
+        'de': 'Zufällig',
+        'en': 'Random',
+    },
+    'ascending': {
+        'de': 'Ascending',
+        'en': 'Aufsteigend',
+    },
+    'descending': {
+        'de': 'Descending',
+        'en': 'Absteigend',
+    },
+    'create_array': {
+        'de': 'Feld erzeugen',
+        'en': 'Create array',       
+    },
+    'interval_of_sorting_steps': {
+        'de': 'Intervall der Sortierschritte',
+        'en': 'Interval of sorting steps',  
+    },
+    'start_sorting': {
+        'de': 'Sortieren starten',
+        'en': 'Start sorting',  
+    },
+    'stop_sorting': {
+        'de': 'Sortieren stoppen',
+        'en': 'Stop sorting',  
+    },
+    'amount_comparisons': {
+        'de': 'Anzahl der Vergleiche: &thinsp;&thinsp;&thinsp; 0',
+        'en': 'Amount comparisons:  &thinsp;&thinsp;&thinsp;&thinsp; 0',  
+    },
+    'amount_array_accesses': {
+        'de': 'Anzahl der Feldzugriffe : 0',
+        'en': 'Amount array accesses: 0',  
+    }
+};
