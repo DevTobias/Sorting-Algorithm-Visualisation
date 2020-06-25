9384be79318abdc44ef1eebb0b3b3751db920ed0
Tobias Kärst <tobias.kaerst@gmx.net>
2020-06-25 04:26:06 +0200

    Animationframe implementation, sorting animation

--- scripts/sorting_visualizer.js
+++ scripts/sorting_visualizer.js
@@ -80,7 +80,12 @@ class SortingVisualizer {
         this.amount_elements; this.sorting_mode; this.key_mode;
         this.delay; this.sound_volume; this.array;
 
+        this.bar_height; this.bar_width; this.distance = 2;
+
         this.perm_queue = new Queue();
+
+        this.fps = 2; this.fpsInterval; this.startTime;
+        this.now; this.then; this.elapsed;
     }
 
     reload_settings() {
@@ -92,48 +97,45 @@ class SortingVisualizer {
         this.array = ArrayUtils.get_array(this.sorting_mode, this.key_mode, this.amount_elements); 
     }
 
-    render() {
+    render_array() {
 
         // Import SVG and empty the old one
         var svgns = 'http://www.w3.org/2000/svg';
         $('#bar_svg').empty();
         
         // Load the Canvas and the SVG and FontSize for Calculations
-        var svg_img = document.getElementById('bar_svg');
-        var canvas_element = document.getElementById('svg_box');
-        var canvas_font_size = parseFloat(getComputedStyle(canvas_element).fontSize);
-        var svg_width = canvas_element.getBoundingClientRect().width - 2 * canvas_font_size;
-        var svg_height = canvas_element.getBoundingClientRect().height - 2 * canvas_font_size;
-    
+        let svg_img = document.getElementById('bar_svg');
+        let canvas_element = document.getElementById('svg_box');
+        let canvas_font_size = parseFloat(getComputedStyle(canvas_element).fontSize);
+        let svg_width = canvas_element.getBoundingClientRect().width - 2 * canvas_font_size;
+        let svg_height = canvas_element.getBoundingClientRect().height - 2 * canvas_font_size;
+           
+        // Style of the rectangles
+        this.distance = 1.5;
+        if(this.amount_elements > 500) this.distance = 0;
+        else if(this.amount_elements > 300) this.distance = 1;
+        else if(this.amount_elements > 150) this.distance = 0.5;
+
         // Calculate the needed Information for the display
-        var bar_width = svg_width / this.amount_elements;
-        var bar_height = svg_height / this.amount_elements;
+        this.bar_width = (svg_width - this.distance * (this.amount_elements - 2) ) / this.amount_elements;
+        this.bar_height = svg_height / this.amount_elements;
     
         // Set the Dimensions of the svg image
         svg_img.setAttribute('width', svg_width);
         svg_img.setAttribute('height', svg_height);
     
-        var current_width = 0; var counter = 0;
-        for(var i = 0; i < this.amount_elements; ++i) {
+        let current_width = 0; let counter = 0;
+        for(let i = 0; i < this.amount_elements; ++i) {
     
             // Create new rectangle and modify the attributes 
-            var rect = document.createElementNS(svgns, 'rect');
+            let rect = document.createElementNS(svgns, 'rect');
             rect.id = "rect_" + counter++;
             rect.setAttributeNS(null, 'x', current_width);
             rect.setAttributeNS(null, 'y', 0);
-            rect.setAttributeNS(null, 'height', this.array[i] * bar_height);
-            rect.setAttributeNS(null, 'width', bar_width);
-            current_width += bar_width;
-    
-            // Style of the rectangles
-            rect.setAttributeNS(null, 'stroke', '#fff');
+            rect.setAttributeNS(null, 'height', this.array[i] *  this.bar_height);
+            rect.setAttributeNS(null, 'width', this.bar_width);
+            current_width += this.bar_width + this.distance;
 
-            let stroke = '1.5';
-            if(this.amount_elements > 500) stroke = '0';
-            else if(this.amount_elements > 300) stroke = '0.5';
-            else if(this.amount_elements > 150) stroke = '1';
-
-            rect.setAttributeNS(null, 'stroke-width', stroke);
             rect.setAttributeNS(null, 'fill', this.DEFAULT_COLOR);
             
             // Render it on screen
@@ -143,8 +145,81 @@ class SortingVisualizer {
     
     }
 
+
+    start_animation() {
+        this.fpsInterval = 1000 / this.fps;
+        this.then = Date.now();
+        this.startTime = this.then;
+        this.animate();
+    }
+
+    animate() {
+       requestAnimationFrame(()=>this.animate());
+
+        this.now = Date.now();
+        this.elapsed = this.now - this.then;
+
+        if (this.elapsed > this.fpsInterval) {
+            this.then = this.now - (this.elapsed % this.fpsInterval);
+            this.evaluate_permutation();
+        }
+    }
+
+    evaluate_permutation() {
+        let perm = this.perm_queue.dequeue();
+        let operation = perm[0]; let i = perm[1]; let j = perm[2];
+
+        switch(operation) {
+
+            case 'swap': {
+
+                let rect1 = document.getElementById('rect_' + i);
+                let rect2 = document.getElementById('rect_' + j);
+
+                rect1.setAttributeNS(null, 'fill', this.SWAP_COLOR);
+                rect2.setAttributeNS(null, 'fill', this.SWAP_COLOR);
+
+                $(rect1).animate({ x: j * this.bar_width + this.distance * j}, this.fpsInterval);
+                $(rect2).animate({ x: i * this.bar_width + this.distance * i}, this.fpsInterval);
+                rect1.id = "rect_" + j; rect2.id = "rect_" + i;
+                
+                break;
+
+            } case 'r_swap': {
+
+                let rect1 = document.getElementById('rect_' + i);
+                let rect2 = document.getElementById('rect_' + j);
+                rect1.setAttributeNS(null, 'fill', this.DEFAULT_COLOR);
+                rect2.setAttributeNS(null, 'fill', this.DEFAULT_COLOR);
+
+                break;
+
+            } case 'comp': {
+
+                let rect1 = document.getElementById('rect_' + i);
+                let rect2 = document.getElementById('rect_' + j);
+                rect1.setAttributeNS(null, 'fill', this.COMPARE_COLOR);
+                rect2.setAttributeNS(null, 'fill', this.COMPARE_COLOR);
+
+                break;
+
+            } case 'r_comp': {
+
+                let rect1 = document.getElementById('rect_' + i);
+                let rect2 = document.getElementById('rect_' + j);
+                rect1.setAttributeNS(null, 'fill', this.DEFAULT_COLOR);
+                rect2.setAttributeNS(null, 'fill', this.DEFAULT_COLOR);
+
+                break;
+            }
+
+        }
+    }
+
+
     swap(i, j) { 
         this.perm_queue.enqueue(['swap', i, j]); 
+        this.perm_queue.enqueue(['r_swap', i, j]); 
         
         let temp_value = this.array[i];
         this.array[i] = this.array[j];
@@ -153,7 +228,8 @@ class SortingVisualizer {
 
     compare(i, j) { 
         this.perm_queue.enqueue(['comp', i, j]); 
-    
+        this.perm_queue.enqueue(['r_comp', i, j]); 
+
         if(this.array[i] > this.array[j]) return 1;
         else if(this.array[i] < this.array[j]) return -1;
         else return 0;
@@ -185,18 +261,17 @@ class SortingVisualizer {
 var visualizer = new SortingVisualizer();
 
 function render() {
-    visualizer.render();
+    visualizer.render_array();
 }
 
 function render_new_array() {
     visualizer.reload_settings();
-    visualizer.render();
+    visualizer.render_array();
 }
 
 function start_sorting() {
     SortingAlgorithms.bubble_sort();
-    visualizer.render();
-    console.log(visualizer.perm_queue);
+    visualizer.start_animation();
 }
 
 $(function() {
